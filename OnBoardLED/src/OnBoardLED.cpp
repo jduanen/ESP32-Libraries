0b731c2b737396f3effca3d064e4f961316893d6
Duane Northcutt <duane.northcutt@gmail.com>
2024-07-25 12:51:28 -0700

    First cut. Doesn't have brightness support for NeoPixels yet.

--- OnBoardLED/src/OnBoardLED.cpp
+++ OnBoardLED/src/OnBoardLED.cpp
@@ -9,17 +9,40 @@
 
 OnBoardLED::OnBoardLED(uint8_t ledPin) {
 	_ledPin = ledPin;
+
 	pinMode(_ledPin, OUTPUT);
+
+	setColor(BLACK);
+
 	_mode = MONO_LED_MODE;
 };
+
+OnBoardLED::OnBoardLED(uint8_t neoLedPwrPin, uint8_t neoLedCmdPin) {
+	_pwrPin = neoLedPwrPin;
+	_cmdPin = neoLedCmdPin;
+
+	pinMode(_pwrPin, OUTPUT);
+	pinMode(_cmdPin, OUTPUT);
+
+////	#ifdef XIAO_RP2040  //// FIXME
+	_neoPixel = new Adafruit_NeoPixel(1, _cmdPin, NEO_GBR + NEO_KHZ800);
+	digitalWrite(_pwrPin, HIGH);
+	_neoPixel->clear();
+
+	_mode = NEO_PIXEL_MODE;
+}
 
 OnBoardLED::OnBoardLED(uint8_t redPin, uint8_t grnPin, uint8_t bluPin) {
 	_redPin = redPin;
 	_grnPin = grnPin;
 	_bluPin = bluPin;
+
 	pinMode(_redPin, OUTPUT);
 	pinMode(_grnPin, OUTPUT);
 	pinMode(_bluPin, OUTPUT);
+
+	setColor(BLACK);
+
 	_mode = RGB_LED_MODE;
 };
 
@@ -35,13 +58,26 @@ void OnBoardLED::setColor(LEDColors color) {
 	switch (_mode) {
 	case MONO_LED_MODE:
 		digitalWrite(_ledPin, color);
-		_ledState = (color != 0);
+		_ledColor = (color == 0) ? BLACK : WHITE;
 		break;
 	case RGB_LED_MODE:
 		digitalWrite(_redPin, (color & 0x01));
 		digitalWrite(_grnPin, ((color >> 1) & 0x01));
 		digitalWrite(_bluPin, ((color >> 2) & 0x01));
-		_ledState = color;
+		_ledColor = color;
+		break;
+	case NEO_PIXEL_MODE:
+		//// TODO add brightness control
+		uint32_t R = (color & 0x01) ? 0x00001F : 0x000000;
+		uint32_t G = ((color >> 1) & 0x01) ? 0x001F00 : 0x000000;
+		uint32_t B = ((color >> 2) & 0x01) ? 0x1F0000 : 0x000000;
+		_neoPixel->setPixelColor(0, (R | G | B));
+		_neoPixel->show();
 		break;
 	}
+	_ledColor = color;
 };
+
+LEDColors OnBoardLED::currentColor() {
+	return _ledColor;
+}
--- OnBoardLED/src/OnBoardLED.h
+++ OnBoardLED/src/OnBoardLED.h
@@ -1,6 +1,17 @@
 /********************************************************************************
  * 
  * Xiao Device On-Board LED Control Library
+ * 
+ * Xiao RP2040 has a power LED (Red), a user-programmable RGB LED, and a NEOPixel.
+ *   LED_BUILTIN = 17
+ *   PIN_LED_R = 17
+ *   PIN_LED_G = 16
+ *   PIN_LED_B = 25
+ *   NeoPixel:
+ *     - PIN_NEOPIXEL   = 12  // command pin
+ *     - NEOPIXEL_POWER = 11  // power pin
+ *
+ * These are defined in pins_arduino.h
  *
  ********************************************************************************/
 
@@ -8,9 +19,12 @@
 
 #include <stdint.h>
 
+#include <Adafruit_NeoPixel.h>
+
 
 #define MONO_LED_MODE	0
 #define RGB_LED_MODE	1
+#define NEO_PIXEL_MODE	2
 
 
 typedef enum LEDColors_e {
@@ -28,14 +42,20 @@ typedef enum LEDColors_e {
 class OnBoardLED {
 public:
 	OnBoardLED(uint8_t ledPin);
+	OnBoardLED(uint8_t neoLedPwrPin, uint8_t neoLedCmdPin);
 	OnBoardLED(uint8_t redPin, uint8_t grnPin, uint8_t bluPin);
 
 	void on();
 	void off();
 	void setColor(LEDColors color);
+
+	LEDColors currentColor();
 protected:
 	uint8_t _mode;
 	uint8_t _ledPin;
-	uint8_t _redPin, _grnPin, bluPin;
-	LEDColors _ledState = BLACK;
+	uint8_t _redPin, _grnPin, _bluPin;
+	uint8_t _pwrPin, _cmdPin;
+	int8_t _brightness = 0x7F;
+	LEDColors _ledColor = BLACK;
+	Adafruit_NeoPixel *_neoPixel;
 };
